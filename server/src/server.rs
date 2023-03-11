31feb45c67e6bc36ed2c5a5f01c367d09c820618
Suguru Yamamoto <sgryjp@gmail.com>
2023-03-11 20:57:09 +0900

    Handle textDocument/didOpen

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,11 +1,20 @@
-use lsp_server::Connection;
-use lsp_types::InitializeParams;
+use lsp_server::{Connection, Message::Notification};
+use lsp_types::{DidOpenTextDocumentParams, InitializeParams};
 
 use crate::errors::JournalintError;
 
 pub fn main_loop(conn: &Connection, init_params: &InitializeParams) -> Result<(), JournalintError> {
     for msg in &conn.receiver {
-        eprintln!("# msg: {:?}", msg);
+        if let Notification(notif) = msg {
+            if notif.method == "textDocument/didOpen" {
+                let params: DidOpenTextDocumentParams = serde_json::from_value(notif.params)?;
+                on_text_document_did_open(params);
+            }
+        }
     }
     Ok(())
+}
+
+fn on_text_document_did_open(params: DidOpenTextDocumentParams) {
+    eprintln!("DidOpenTextDocumentParams: {:?}", params);
 }
