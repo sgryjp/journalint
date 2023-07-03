9826561dd18a1c64470d4a0bac6aadb9c0d456f9
Suguru Yamamoto <sgryjp@gmail.com>
2023-07-04 00:26:09 +0900

    build: Fix Node target name

--- tools/journalint-vscode/scripts/compile.js
+++ tools/journalint-vscode/scripts/compile.js
@@ -3,13 +3,13 @@ const process = require("process");
 const shell = require("shelljs");
 
 const targetMapping = new Map([
-  ["linux_x64", "x86_64-unknown-linux-musl"],
-  ["darwin_arm64", "aarch64-apple-darwin"],
-  ["win32_x64", "x86_64-pc-windows-msvc"],
+  ["linux-x64", "x86_64-unknown-linux-musl"],
+  ["darwin-arm64", "aarch64-apple-darwin"],
+  ["win32-x64", "x86_64-pc-windows-msvc"],
 ]);
 
 // Resolve path to the native binary
-const nodeTarget = `${process.platform}_${process.arch}`;
+const nodeTarget = `${process.platform}-${process.arch}`;
 console.log("nodeTarget:", nodeTarget);
 shell.set("-ev");
 shell.exec("tsc -p ./");
