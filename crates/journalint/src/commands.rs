bfc6b86df503c712a7ca17295eedba8ce1f60d35
Suguru Yamamoto <sgryjp@gmail.com>
2023-09-16 15:42:08 +0900

    feat: Support fixing time-jumped

--- crates/journalint/src/commands.rs
+++ crates/journalint/src/commands.rs
@@ -10,7 +10,8 @@ use crate::errors::JournalintError;
 use crate::service::ServerState;
 
 pub const RECALCULATE_DURATION: &str = "journalint.recalculateDuration";
-pub const ALL_COMMANDS: [&str; 1] = [RECALCULATE_DURATION];
+pub const REPLACE_WITH_PREVIOUS_END_TIME: &str = "journalint.replaceWithPreviousEndTime";
+pub const ALL_COMMANDS: [&str; 2] = [RECALCULATE_DURATION, REPLACE_WITH_PREVIOUS_END_TIME];
 
 pub trait Command {
     fn title(&self) -> &str;
@@ -29,6 +30,7 @@ pub struct CommandParams {
     range: lsp_types::Range,
 }
 
+// -----------------------------------------------------------------------------
 #[derive(Debug)]
 pub struct RecalculateDuration {}
 
@@ -54,6 +56,33 @@ impl Command for RecalculateDuration {
     }
 }
 
+// -----------------------------------------------------------------------------
+#[derive(Debug)]
+pub struct ReplaceWithPreviousEndTime {}
+
+impl Command for ReplaceWithPreviousEndTime {
+    fn title(&self) -> &str {
+        "Replace with the previous entry's end time"
+    }
+
+    fn command(&self) -> &str {
+        REPLACE_WITH_PREVIOUS_END_TIME
+    }
+
+    fn execute(
+        &self,
+        state: &ServerState,
+        url: &Url,
+        range: &lsp_types::Range,
+    ) -> Option<WorkspaceEdit> {
+        let Some(diagnostic) = find_diagnostic(state, url, range, Code::TimeJumped) else {
+            return None;
+        };
+        fix_diagnostic(diagnostic, url)
+    }
+}
+
+// -----------------------------------------------------------------------------
 pub fn list_available_code_actions(code: &Code) -> Option<Vec<Box<dyn Command>>> {
     match code {
         Code::ParseError => None,
@@ -63,7 +92,7 @@ pub fn list_available_code_actions(code: &Code) -> Option<Vec<Box<dyn Command>>>
         Code::MissingDate => None,
         Code::MissingStartTime => None,
         Code::MissingEndTime => None,
-        Code::TimeJumped => None,
+        Code::TimeJumped => Some(vec![Box::new(ReplaceWithPreviousEndTime {})]),
         Code::NegativeTimeRange => None,
         Code::IncorrectDuration => Some(vec![Box::new(RecalculateDuration {})]),
     }
@@ -72,6 +101,7 @@ pub fn list_available_code_actions(code: &Code) -> Option<Vec<Box<dyn Command>>>
 pub fn get_command_by_name(name: &str) -> Option<Box<dyn Command>> {
     match name {
         RECALCULATE_DURATION => Some(Box::new(RecalculateDuration {})),
+        REPLACE_WITH_PREVIOUS_END_TIME => Some(Box::new(ReplaceWithPreviousEndTime {})),
         _ => None,
     }
 }
