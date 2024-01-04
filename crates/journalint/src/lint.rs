154f3a36e741b7b17456040745b389e17dcd369e
Suguru Yamamoto <sgryjp@gmail.com>
2024-01-05 01:35:48 +0900

    refactor: Extract check logic out

--- crates/journalint/src/lint.rs
+++ crates/journalint/src/lint.rs
@@ -48,20 +48,15 @@ impl<'a> Linter<'a> {
             prev_entry_end: None,
         }
     }
-}
 
-impl Visitor for Linter<'_> {
-    fn on_visit_fm_date(&mut self, value: &NaiveDate, span: &Range<usize>) {
-        let value = *value;
-        self.fm_date = Some((value, span.clone()));
-
-        // Check the date value matches the one in the file name
+    // Check if date value in the front matter does not match the one in the filename
+    fn check_fm_date_matches_filename(&mut self, value: &NaiveDate, span: &Range<usize>) {
         if let Some(stem) = PathBuf::from(self.source.path())
             .file_stem()
             .and_then(|s| s.to_str())
         {
             if let Ok(date_in_filename) = NaiveDate::parse_from_str(stem, "%Y-%m-%d") {
-                if date_in_filename != value {
+                if date_in_filename != *value {
                     let expectation = date_in_filename.format("%Y-%m-%d").to_string();
                     self.diagnostics.push(Diagnostic::new_warning(
                         span.clone(),
@@ -79,54 +74,7 @@ impl Visitor for Linter<'_> {
         }
     }
 
-    fn on_visit_fm_start(&mut self, value: &LooseTime, span: &Range<usize>) {
-        self.fm_start = Some((value.clone(), span.clone()));
-    }
-
-    fn on_visit_fm_end(&mut self, value: &LooseTime, span: &Range<usize>) {
-        self.fm_end = Some((value.clone(), span.clone()));
-    }
-
-    fn on_leave_fm(&mut self, _date: &Expr, _start: &Expr, _end: &Expr, span: &Range<usize>) {
-        // Calculate exact time of start and end
-        if let (Some((date, _)), Some((start, start_span))) =
-            (self.fm_date.as_ref(), self.fm_start.as_ref())
-        {
-            self.fm_start_datetime = match start.to_datetime(*date) {
-                Ok(dt) => Some(dt),
-                Err(e) => {
-                    self.diagnostics.push(Diagnostic::new_warning(
-                        start_span.clone(),
-                        Code::InvalidStartTime,
-                        format!("Invalid start time: {e}"),
-                        None,
-                        None,
-                        self.line_map.clone(),
-                    ));
-                    None
-                }
-            };
-        }
-        if let (Some((date, _)), Some((end, end_span))) =
-            (self.fm_date.as_ref(), self.fm_end.as_ref())
-        {
-            self.fm_end_datetime = match end.to_datetime(*date) {
-                Ok(dt) => Some(dt),
-                Err(e) => {
-                    self.diagnostics.push(Diagnostic::new_warning(
-                        end_span.clone(),
-                        Code::InvalidEndTime,
-                        format!("Invalid end time: {e}"),
-                        None,
-                        None,
-                        self.line_map.clone(),
-                    ));
-                    None
-                }
-            };
-        }
-
-        // Warn if one of date, start and end is missing
+    fn check_fm_date_exists(&mut self, span: &Range<usize>) {
         if self.fm_date.is_none() {
             self.diagnostics.push(Diagnostic::new_warning(
                 span.clone(),
@@ -137,6 +85,9 @@ impl Visitor for Linter<'_> {
                 self.line_map.clone(),
             ));
         }
+    }
+
+    fn check_fm_start_exists(&mut self, span: &Range<usize>) {
         if self.fm_start.is_none() {
             self.diagnostics.push(Diagnostic::new_warning(
                 span.clone(),
@@ -147,6 +98,33 @@ impl Visitor for Linter<'_> {
                 self.line_map.clone(),
             ));
         }
+    }
+
+    fn check_fm_start_is_valid(&mut self) -> Option<DateTime<Utc>> {
+        let Some((date, _)) = self.fm_date.as_ref() else {
+            return None;
+        };
+        let Some((start, start_span)) = self.fm_start.as_ref() else {
+            return None;
+        };
+
+        match start.to_datetime(*date) {
+            Ok(dt) => Some(dt),
+            Err(e) => {
+                self.diagnostics.push(Diagnostic::new_warning(
+                    start_span.clone(),
+                    Code::InvalidStartTime,
+                    format!("Invalid start time: {e}"),
+                    None,
+                    None,
+                    self.line_map.clone(),
+                ));
+                None
+            }
+        }
+    }
+
+    fn check_fm_end_exists(&mut self, span: &Range<usize>) {
         if self.fm_end.is_none() {
             self.diagnostics.push(Diagnostic::new_warning(
                 span.clone(),
@@ -159,105 +137,196 @@ impl Visitor for Linter<'_> {
         }
     }
 
+    fn check_fm_end_is_valid(&mut self) -> Option<DateTime<Utc>> {
+        let Some((date, _)) = self.fm_date.as_ref() else {
+            return None;
+        };
+        let Some((end, end_span)) = self.fm_end.as_ref() else {
+            return None;
+        };
+
+        match end.to_datetime(*date) {
+            Ok(dt) => Some(dt),
+            Err(e) => {
+                self.diagnostics.push(Diagnostic::new_warning(
+                    end_span.clone(),
+                    Code::InvalidEndTime,
+                    format!("Invalid end time: {e}"),
+                    None,
+                    None,
+                    self.line_map.clone(),
+                ));
+                None
+            }
+        }
+    }
+
+    // Check if start time matches the end of the previous entry
+    fn check_prev_end_equals_next_start(&mut self, start_dt: DateTime<Utc>, span: &Range<usize>) {
+        if let Some((prev_end_dt, prev_end_range)) = self.prev_entry_end.as_ref() {
+            if start_dt != *prev_end_dt {
+                let expectation = prev_end_dt.format("%H:%M").to_string();
+                self.diagnostics.push(Diagnostic::new_warning(
+                    span.clone(),
+                    Code::TimeJumped,
+                    format!("The start time does not match the previous entry's end time, which is {expectation}"),
+                    Some(expectation),
+                    Some(vec![DiagnosticRelatedInformation::new(
+                        self.source.clone(),
+                        prev_end_range.clone(),
+                        format!(
+                            "Previous entry's end time is {:02}:{:02}",
+                            prev_end_dt.hour(),
+                            prev_end_dt.minute()
+                        ),
+                        self.line_map.clone(),
+                    )]),
+                    self.line_map.clone(),
+                ));
+            }
+        }
+    }
+
+    fn check_start_time(
+        &mut self,
+        value: &LooseTime,
+        span: &Range<usize>,
+    ) -> Option<DateTime<Utc>> {
+        let Some((date, _)) = self.fm_date else {
+            return None;
+        };
+
+        match value.to_datetime(date) {
+            Ok(dt) => Some(dt),
+            Err(e) => {
+                // Start time is not a valid value
+                self.diagnostics.push(Diagnostic::new_warning(
+                    span.clone(),
+                    Code::InvalidStartTime,
+                    format!("Invalid start time: {e}"),
+                    None,
+                    None,
+                    self.line_map.clone(),
+                ));
+                None
+            }
+        }
+    }
+
+    fn check_end_time(&mut self, value: &LooseTime, span: &Range<usize>) -> Option<DateTime<Utc>> {
+        let Some((date, _)) = self.fm_date else {
+            return None;
+        };
+
+        match value.to_datetime(date) {
+            Ok(dt) => Some(dt),
+            Err(e) => {
+                self.diagnostics.push(Diagnostic::new_warning(
+                    span.clone(),
+                    Code::InvalidEndTime,
+                    format!("Invalid end time: {e}"),
+                    None,
+                    None,
+                    self.line_map.clone(),
+                ));
+                None
+            }
+        }
+    }
+
+    fn check_end_time_exceeds_start_time(&mut self) {
+        let Some((start, _)) = self.entry_start.as_ref() else {
+            return;
+        };
+        let Some((end, end_span)) = self.entry_end.as_ref() else {
+            return;
+        };
+
+        let Ok(_) = (*end - *start).to_std() else {
+            self.diagnostics.push(Diagnostic::new_warning(
+                end_span.clone(),
+                Code::NegativeTimeRange,
+                format!(
+                    "End time is not ahead of start time ({})",
+                    start.format("%H:%M"),
+                ),
+                None,
+                None,
+                self.line_map.clone(),
+            ));
+            return;
+        };
+    }
+
+    fn check_duration_matches_end_minus_start(&mut self, value: &Duration, span: &Range<usize>) {
+        let Some((start, _)) = self.entry_start.as_ref() else {
+            return;
+        };
+        let Some((end, _)) = self.entry_end.as_ref() else {
+            return;
+        };
+        let Ok(calculated) = (*end - *start).to_std() else {
+            return;
+        };
+        let written = value;
+        if calculated != *written {
+            let expectation = calculated.as_secs_f64() / 3600.0;
+            let expectation = format!("{expectation:1.2}");
+            self.diagnostics.push(Diagnostic::new_warning(
+                span.clone(),
+                Code::IncorrectDuration,
+                format!("Incorrect duration: expected {expectation}"),
+                Some(expectation),
+                None,
+                self.line_map.clone(),
+            ));
+        }
+    }
+}
+
+impl Visitor for Linter<'_> {
+    fn on_visit_fm_date(&mut self, value: &NaiveDate, span: &Range<usize>) {
+        self.fm_date = Some((*value, span.clone()));
+        self.check_fm_date_matches_filename(value, span)
+    }
+
+    fn on_visit_fm_start(&mut self, value: &LooseTime, span: &Range<usize>) {
+        self.fm_start = Some((value.clone(), span.clone()));
+    }
+
+    fn on_visit_fm_end(&mut self, value: &LooseTime, span: &Range<usize>) {
+        self.fm_end = Some((value.clone(), span.clone()));
+    }
+
+    fn on_leave_fm(&mut self, _date: &Expr, _start: &Expr, _end: &Expr, span: &Range<usize>) {
+        // Calculate exact time of start and end
+        self.fm_start_datetime = self.check_fm_start_is_valid();
+        self.fm_end_datetime = self.check_fm_end_is_valid();
+
+        // Warn if one of date, start and end is missing
+        self.check_fm_date_exists(span);
+        self.check_fm_start_exists(span);
+        self.check_fm_end_exists(span);
+    }
+
     fn on_visit_entry(&mut self, _span: &Range<usize>) {}
 
     fn on_visit_start_time(&mut self, value: &LooseTime, span: &Range<usize>) {
-        if let Some((date, _)) = self.fm_date {
-            match value.to_datetime(date) {
-                Ok(start_dt) => {
-                    self.entry_start = Some((start_dt, span.clone()));
-
-                    // Check if start time matches the end of the previous entry
-                    if let Some((prev_end_dt, prev_end_range)) = self.prev_entry_end.as_ref() {
-                        if start_dt != *prev_end_dt {
-                            let expectation = prev_end_dt.format("%H:%M").to_string();
-                            self.diagnostics.push(Diagnostic::new_warning(
-                                span.clone(),
-                                Code::TimeJumped,
-                                format!("The start time does not match the previous entry's end time, which is {expectation}"),
-                                Some(expectation),
-                                Some(vec![DiagnosticRelatedInformation::new(
-                                    self.source.clone(),
-                                    prev_end_range.clone(),
-                                    format!(
-                                        "Previous entry's end time is {:02}:{:02}",
-                                        prev_end_dt.hour(),
-                                        prev_end_dt.minute()
-                                    ),
-                                    self.line_map.clone(),
-                                )]),
-                                self.line_map.clone(),
-                            ));
-                        }
-                    }
-                }
-                Err(e) => {
-                    // Start time is not a valid value
-                    self.diagnostics.push(Diagnostic::new_warning(
-                        span.clone(),
-                        Code::InvalidStartTime,
-                        format!("Invalid start time: {e}"),
-                        None,
-                        None,
-                        self.line_map.clone(),
-                    ));
-                }
-            };
+        if let Some(start_dt) = self.check_start_time(value, span) {
+            self.entry_start = Some((start_dt, span.clone()));
+            self.check_prev_end_equals_next_start(start_dt, span);
         }
     }
 
     fn on_visit_end_time(&mut self, value: &LooseTime, span: &Range<usize>) {
-        if let Some((date, _)) = self.fm_date {
-            match value.to_datetime(date) {
-                Ok(d) => {
-                    self.entry_end = Some((d, span.clone()));
-                }
-                Err(e) => {
-                    self.diagnostics.push(Diagnostic::new_warning(
-                        span.clone(),
-                        Code::InvalidEndTime,
-                        format!("Invalid end time: {e}"),
-                        None,
-                        None,
-                        self.line_map.clone(),
-                    ));
-                }
-            }
+        if let Some(dt) = self.check_end_time(value, span) {
+            self.entry_end = Some((dt, span.clone()));
         }
     }
 
     fn on_visit_duration(&mut self, value: &Duration, span: &Range<usize>) {
-        if let (Some((start, _)), Some((end, end_span))) =
-            (self.entry_start.as_ref(), self.entry_end.as_ref())
-        {
-            let Ok(calculated) = (*end - *start).to_std() else {
-                self.diagnostics.push(Diagnostic::new_warning(
-                    end_span.clone(),
-                    Code::NegativeTimeRange,
-                    format!(
-                        "End time is not ahead of start time ({})",
-                        start.format("%H:%M"),
-                    ),
-                    None,
-                    None,
-                    self.line_map.clone(),
-                ));
-                return;
-            };
-            let written = value;
-            if calculated != *written {
-                let expectation = calculated.as_secs_f64() / 3600.0;
-                let expectation = format!("{expectation:1.2}");
-                self.diagnostics.push(Diagnostic::new_warning(
-                    span.clone(),
-                    Code::IncorrectDuration,
-                    format!("Incorrect duration: expected {expectation}"),
-                    Some(expectation),
-                    None,
-                    self.line_map.clone(),
-                ));
-            }
-        }
+        self.check_end_time_exceeds_start_time();
+        self.check_duration_matches_end_minus_start(value, span);
     }
 
     fn on_visit_code(&mut self, _value: &str, _span: &Range<usize>) {}
