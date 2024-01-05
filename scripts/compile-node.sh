226647e2b0c5bcf07f86ed3c71c31830cd641cde
Suguru Yamamoto <sgryjp@gmail.com>
2024-01-05 13:46:26 +0900

    fix(ci): Wong path

--- scripts/compile-node.sh
+++ scripts/compile-node.sh
@@ -10,5 +10,5 @@ npm cache verify >/dev/null
 npm ci >/dev/null
 npm exec -c 'tsc -p .'
 [ -f $native_executable_path ] || $workspace_dir/scripts/compile-crates.sh
-mkdir -p $workspace_dir/bundles/$node_target
+mkdir -p bundles/$node_target
 cp $native_executable_path bundles/$node_target
